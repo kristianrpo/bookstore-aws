11dea31465c82db59be2cae34c581c921d64cd3e
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-05-01 19:15:23 -0500

    fix: delete methods

--- monolithic/controllers/home_controller.py
+++ monolithic/controllers/home_controller.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 home = Blueprint('home', __name__, url_prefix='/objective-1')
 
-@home.route('/', methods=['GET', 'POST'])
+@home.route('/')
 def home():
     return render_template('home.html')
