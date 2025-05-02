54b49699d2dd8de20caa64e58244535cd1fd780c
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-05-01 19:18:59 -0500

    fix: change name to home function

--- monolithic/controllers/home_controller.py
+++ monolithic/controllers/home_controller.py
@@ -3,5 +3,5 @@ from flask import Blueprint, render_template
 home = Blueprint('home', __name__, url_prefix='/objective-1')
 
 @home.route('/')
-def home():
+def index():
     return render_template('home.html')
