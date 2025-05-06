eff120f0bd57e1d69555a71b96caf19659d213c2
Kristian Restrepo Osorio <krestrepoo@eafit.edu.co>
2025-05-06 18:35:36 -0500

    feat: add book service interface to use dependency injection

---
+++ microservices-refactor/services/book/app/services/interfaces/book.py
@@ -0,0 +1,29 @@
+from abc import ABC, abstractmethod
+
+class BookServiceInterface(ABC):
+    @abstractmethod
+    def catalog():
+        """Retrieve all books."""
+        pass
+
+    @abstractmethod
+    def my_books():
+        """Retrieve all books for a specific seller."""
+        pass
+
+    @abstractmethod
+    def add_book(title: str, author: str, description: str, price: float, stock: int):
+        """Create a new book with the given details."""
+        pass
+
+    @abstractmethod
+    def edit_book(book_id: int, title: str, author: str, description: str, price: float, stock: int):
+        """Update a book with the given attributes."""
+        pass
+
+    @abstractmethod
+    def delete_book(book_id: int):
+        """Delete a specific book given that its book_id."""
+        pass
+
+
