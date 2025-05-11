39affb838adb4f9dd496c7f550ef82563c1d5b9d
Evelyn <eazapatat@eafit.edu.co>
2025-05-11 08:22:56 -0500

    refactor: modify order routes to manage docker networks

--- microservices-refactor/frontend/constants/api.urls.ts
+++ microservices-refactor/frontend/constants/api.urls.ts
@@ -1,29 +1,29 @@
-const NEXT_PUBLIC_BASE_URL_SERVICE_AUTH = process.env.NEXT_PUBLIC_BASE_URL_API_AUTH;
-const NEXT_PUBLIC_BASE_URL_SERVICE_BOOK = process.env.NEXT_PUBLIC_BASE_URL_API_BOOK;
-const NEXT_PUBLIC_BASE_URL_SERVICE_ORDER = process.env.NEXT_PUBLIC_BASE_URL_API_ORDER;
+const BASE_URL_SERVICE_AUTH = process.env.BASE_URL_API_AUTH;
+const BASE_URL_SERVICE_BOOK = process.env.BASE_URL_API_BOOK;
+const BASE_URL_SERVICE_ORDER = process.env.BASE_URL_API_ORDER;
 
 const ROUTES_API = {
   AUTH:{
-    LOGIN: `${NEXT_PUBLIC_BASE_URL_SERVICE_AUTH}/login`,
-    LOGOUT: `${NEXT_PUBLIC_BASE_URL_SERVICE_AUTH}/logout`,
-    REGISTER: `${NEXT_PUBLIC_BASE_URL_SERVICE_AUTH}/register`,
-    GET_USER: `${NEXT_PUBLIC_BASE_URL_SERVICE_AUTH}/user`
+    LOGIN: `${BASE_URL_SERVICE_AUTH}/login`,
+    LOGOUT: `${BASE_URL_SERVICE_AUTH}/logout`,
+    REGISTER: `${BASE_URL_SERVICE_AUTH}/register`,
+    GET_USER: `${BASE_URL_SERVICE_AUTH}/user`
   },
   BOOK:{
-    CATALOG: `${NEXT_PUBLIC_BASE_URL_SERVICE_BOOK}/book/catalog`,
-    MY_BOOKS: `${NEXT_PUBLIC_BASE_URL_SERVICE_BOOK}/book/my_books`,
-    BOOK: (id: string) => `${NEXT_PUBLIC_BASE_URL_SERVICE_BOOK}/book/${id}`,
-    EDIT_BOOK: (id: string) => `${NEXT_PUBLIC_BASE_URL_SERVICE_BOOK}/book/edit_book/${id}`,
-    DELETE_BOOK: (id: string) => `${NEXT_PUBLIC_BASE_URL_SERVICE_BOOK}/book/delete_book/${id}`,
-    ADD: `${NEXT_PUBLIC_BASE_URL_SERVICE_BOOK}/book/add_book`,
-    SERVE_IMAGE: (filename: string) => `${NEXT_PUBLIC_BASE_URL_SERVICE_BOOK}/book/image/${filename}`,
+    CATALOG: `${BASE_URL_SERVICE_BOOK}/book/catalog`,
+    MY_BOOKS: `${BASE_URL_SERVICE_BOOK}/book/my_books`,
+    BOOK: (id: string) => `${BASE_URL_SERVICE_BOOK}/book/${id}`,
+    EDIT_BOOK: (id: string) => `${BASE_URL_SERVICE_BOOK}/book/edit_book/${id}`,
+    DELETE_BOOK: (id: string) => `${BASE_URL_SERVICE_BOOK}/book/delete_book/${id}`,
+    ADD: `${BASE_URL_SERVICE_BOOK}/book/add_book`,
+    SERVE_IMAGE: (filename: string) => `${BASE_URL_SERVICE_BOOK}/book/image/${filename}`,
   },
   ORDER:{
-    PAYMENT: (purchase_id: string) => `${NEXT_PUBLIC_BASE_URL_SERVICE_ORDER}/payment/${purchase_id}`,
-    PURCHASE: (book_id: string) => `${NEXT_PUBLIC_BASE_URL_SERVICE_ORDER}/purchase/buy/${book_id}`,
-    DELIVERY: (purchase_id: string) => `${NEXT_PUBLIC_BASE_URL_SERVICE_ORDER}/delivery/select/${purchase_id}`,
-    MY_DELIVERIES: `${NEXT_PUBLIC_BASE_URL_SERVICE_ORDER}/delivery/my_deliveries`,
-    CREATE_DELIVERY: `${NEXT_PUBLIC_BASE_URL_SERVICE_ORDER}/delivery/create`,
+    PAYMENT: (purchase_id: string) => `${BASE_URL_SERVICE_ORDER}/payment/${purchase_id}`,
+    PURCHASE: (book_id: string) => `${BASE_URL_SERVICE_ORDER}/purchase/buy/${book_id}`,
+    DELIVERY: (purchase_id: string) => `${BASE_URL_SERVICE_ORDER}/delivery/select/${purchase_id}`,
+    MY_DELIVERIES: `${BASE_URL_SERVICE_ORDER}/delivery/my_deliveries`,
+    CREATE_DELIVERY: `${BASE_URL_SERVICE_ORDER}/delivery/create`,
   }
 } as const;
 
--- microservices-refactor/frontend/constants/urls.ts
+++ microservices-refactor/frontend/constants/urls.ts
@@ -12,7 +12,7 @@ const ROUTES = {
     DELIVERY : "/delivery",
     MY_DELIVERIES : "/delivery/my_deliveries",
     CREATE_DELIVERY : "/delivery/create",
-    STATIC_SERVER: process.env.NEXT_PUBLIC_BASE_URL_STATIC_SERVER
+    STATIC_SERVER: process.env.BASE_URL_STATIC_SERVER
   } as const;
   
   export default ROUTES;
